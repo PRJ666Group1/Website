720ff4d2d0b3c9a855569fa8b70104682f93a176
Ramin Amiri <zeuspsn.55555@gmail.com>
2025-02-04 22:14:05 -0500

    Registration

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,29 @@
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import styles from './page.module.css';
-import Link from 'next/link';
-
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <main className="container mx-auto p-8">
-
-      {/* Hero Section */}
+        {/* Hero Section */}
         <div className={styles.hero}>
-
           {/* Left side: Text and Buttons */}
           <div className={styles.textContent}>
             <h1 className={styles.heading}>Take Control of Your Finances with AI-Powered Precision</h1>
-            <p className={styles.subheading}>Our AI-driven budget assistant helps you track spending, set goals, and optimize saving efficiently.</p>
-            
+            <p className={styles.subheading}>
+              Our AI-driven budget assistant helps you track spending, set goals, and optimize saving efficiently.
+            </p>
+
             <div className={styles.buttons}>
               <Link href="/registration">
                 <button className={styles.getStarted}>Get Started</button>
               </Link>
               <Link href="/about">
                 <button className={styles.learnMore}>Learn More</button>
-              </Link> 
+              </Link>
             </div>
           </div>
 
@@ -32,31 +31,37 @@ export default function Home() {
           <div className={styles.imageContent}>
             <img src="/assets/dollar.svg" alt="Finance Management" className={styles.image} />
           </div>
-
         </div>
 
-        <hr className={styles.hr}/>
+        <hr className={styles.hr} />
 
         {/* Features Section */}
         <div className={styles.features}>
           <div className={styles.featureItem}>
             <img src="/assets/check.svg" alt="Feature 1" className={styles.featureImage} />
             <h3 className={styles.featureTitle}>Advanced AI Insights</h3>
-            <p className={styles.featureText}>AI-powered tailored personalized insights that are based on your spending habits and financial situation to optimize spending and help you reach your financial goals.</p>
+            <p className={styles.featureText}>
+              AI-powered tailored personalized insights that are based on your spending habits and financial situation
+              to optimize spending and help you reach your financial goals.
+            </p>
           </div>
           <div className={styles.featureItem}>
             <img src="/assets/flag.svg" alt="Feature 2" className={styles.featureImage} />
             <h3 className={styles.featureTitle}>Dynamic Budget Adjustment</h3>
-            <p className={styles.featureText}>Automatically update your budget as your financial situation changes in real time</p>
+            <p className={styles.featureText}>
+              Automatically update your budget as your financial situation changes in real time
+            </p>
           </div>
           <div className={styles.featureItem}>
             <img src="/assets/star.svg" alt="Feature 3" className={styles.featureImage} />
             <h3 className={styles.featureTitle}>Goal-Based Planning</h3>
-            <p className={styles.featureText}>Set and monitor financial goals, with personalized advice to help you achieve them</p>
+            <p className={styles.featureText}>
+              Set and monitor financial goals, with personalized advice to help you achieve them
+            </p>
           </div>
         </div>
 
-        <hr className={styles.hr}/>
+        <hr className={styles.hr} />
 
         <div className={styles.downloadSection}>
           <Link href="/download">
@@ -64,15 +69,10 @@ export default function Home() {
           </Link>
           <div className={styles.downloadImageContainer}>
             <Link href="/download">
-              <img 
-                src="/assets/download.svg" 
-                alt="Desktop App Image" 
-                className={styles.downloadImage} 
-              />
+              <img src="/assets/download.svg" alt="Desktop App Image" className={styles.downloadImage} />
             </Link>
           </div>
         </div>
-
       </main>
       <Footer />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
 // src/components/Header.js
-import Link from 'next/link';
-import styles from './Header.module.css';  // Import the CSS Module
+import Link from "next/link";
+import styles from "./Header.module.css"; // Import the CSS Module
+import Image from "next/image";
+import { Container } from "@mantine/core";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.container}> 
-        <Link href="/">
-          <h1 className={styles.title}>MoneyMap</h1>
-        </Link>
+        <div className={styles.container}>
+          <Link href="/">
+            {/* <h1 className={styles.title}>MoneyMap</h1> */}
+            <Image className={styles.logo} src="/assets/moneymap_logo.png" alt="MoneyMap" width={216} height={34} />
+          </Link>
 
-        <nav className={styles.nav}> 
-          <ul className={styles.navList}> 
-          {/**
+          <nav className={styles.nav}>
+            <ul className={styles.navList}>
+              {/**
             <li>
               <Link href="#packages" className={styles.link}>
                 Packages
@@ -24,29 +27,29 @@ const Header = () => {
               </Link>
             </li>
              */}
-            <li>
-              <Link href="/about" className={styles.link}>
-                About
-              </Link>
-            </li>
-            <li>
-              <Link href="/download" className={styles.link}>
-                Download
-              </Link>
-            </li>
-            <li>
-              <Link href="/registration" className={styles.link}>
-                Registration
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact" className={styles.link}>
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+              <li>
+                <Link href="/about" className={styles.link}>
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link href="/download" className={styles.link}>
+                  Download
+                </Link>
+              </li>
+              <li>
+                <Link href="/registration" className={styles.link}>
+                  Registration
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact" className={styles.link}>
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
     </header>
   );
 };
